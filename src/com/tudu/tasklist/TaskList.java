af819dd3928060e2adf09920632327c58466ebd9
Yash Seeta <yashseeta94@gmail.com>
2021-02-23 20:34:25 +0100

    Added empty class file and its broad outline

---
+++ src/com/tudu/tasklist/TaskList.java
@@ -0,0 +1,9 @@
+package com.tudu.tasklist;
+
+public class TaskList {
+    // Probably a set Collection of Tasks is TaskList
+    // Private fields
+    // Methods : sortByDate(ascending/descending), sortByProjectFlag(ascending/descending), displayTaskList,
+    // Methods : addTask, editTask, markAsDone, removeTask(IF: support removeAll, IF: support removeAllProjectFlag)
+    // Methods : saveToFile (as CSV/TSV), loadFromFile(from CSV/TSV)
+}
