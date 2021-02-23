1366a3b0d6738476c7619f81c03f6895c9d716ab
Yash Seeta <yashseeta94@gmail.com>
2021-02-23 20:33:07 +0100

    Added empty class file and its broad outline

---
+++ src/com/tudu/task/Task.java
@@ -0,0 +1,10 @@
+package com.tudu.task;
+
+public class Task{
+    // Task class contains : title, dueDate, TaskStatus, projectFlag, IF:project color
+    // All fields are private
+    // Getters and setters for all fields
+    // toString Method to display
+    // IF: add colored display based on project color
+    // IF: add a font style : Italics for finished, Bold for those that need to be done, normal for ongoing
+}
