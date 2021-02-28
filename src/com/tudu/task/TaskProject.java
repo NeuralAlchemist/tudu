39fb52e78a4f995e980e2879cc2b7d564a8852ec
Yash Seeta <yashseeta94@gmail.com>
2021-02-28 17:31:56 +0100

    closes #2; Initialised TaskProject
    TaskProject now contains color and name fields

---
+++ src/com/tudu/task/TaskProject.java
@@ -0,0 +1,31 @@
+package com.tudu.task;
+
+public class TaskProject {
+    private String name, color;
+
+    public TaskProject(String name) {
+        this.name = name;
+    }
+
+    public TaskProject(String name, Boolean pickColor){
+        this.name = name;
+        // IF: keep track of colors used (this should be read and written to a file)
+        // Display available colors and read the chosen color
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
