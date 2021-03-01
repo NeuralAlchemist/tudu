567f1f1f2e6ff2cd54402910d1531e645c790799
Yash Seeta <yashseeta94@gmail.com>
2021-03-01 07:56:48 +0100

    Fixes #1
    TaskList is now a TreeMap with key project and a value of array of unsorted tasks. This automatically sorts project by ascending order

--- src/com/tudu/tasklist/TaskList.java
+++ src/com/tudu/tasklist/TaskList.java
@@ -1,30 +1,38 @@
 package com.tudu.tasklist;
 import com.tudu.task.Task;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.Map.Entry;
+import java.util.*;
 
 public class TaskList {
     // Probably a set Collection of Tasks is TaskList
     // Private fields
-    private Set<Task> taskList;
+    private SortedMap<String, ArrayList<Task>> taskList;
     public TaskList() {
-        taskList = new HashSet<Task>(100);
+        taskList = new TreeMap<>();
     }
     // Methods : addTask, editTask, markAsDone, removeTask(IF: support removeAll, IF: support removeAllProjectFlag)
     public void addTask(Task task){
-        if(taskList.contains(task)){
+        String projectName = task.getProject();
+        Boolean projectExistsAlready = taskList.containsKey(projectName);
+        ArrayList<Task> tasksOfProject = projectExistsAlready ? taskList.get(projectName) : new ArrayList<>(100);
+        if(projectExistsAlready && tasksOfProject.contains(task)){
             System.out.println("Task is already present!");
-        } else {
-            System.out.println(task.toString()+"\nAbove task is now in your TuDu list");
-            taskList.add(task);
+        } else if(!tasksOfProject.contains(task)) {
+            // Improve adding to sort it out by dueDate
+            tasksOfProject.add(task);
+            taskList.put(projectName, tasksOfProject);
+            System.out.println("Added Task!");
         }
-
     }
 
     public void displayTaskList(){
-        for (Task value : taskList) {
-            System.out.println(value.toString());
+        System.out.println("Size of tree map: "+taskList.size());
+        for( Map.Entry<String, ArrayList<Task>> entry : taskList.entrySet()){
+            ArrayList<Task> tasksOfProject = entry.getValue();
+            System.out.println("tasks of project: "+entry.getKey()+" total: "+tasksOfProject.size());
+            for(Task task : tasksOfProject){
+                System.out.println(task.toString());
+            }
         }
     }
     // Methods : sortByDate(ascending/descending), sortByProjectFlag(ascending/descending), displayTaskList,
