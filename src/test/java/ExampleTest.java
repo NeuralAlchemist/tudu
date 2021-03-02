d28010f7994ad547ba97602b5e35faf8c916b6c8
Yash Seeta <yashseeta94@gmail.com>
2021-03-02 22:08:26 +0100

    Checked JUnit with Gradle works

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void checkSimpleAssertion() {
+        Assertions.assertEquals(1, 1);
+    }
+}
