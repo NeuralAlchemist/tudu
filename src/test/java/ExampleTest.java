d3577c4c0c6a313a72c4c25aafcbee8c433eae90
Yash Seeta <yashseeta94@gmail.com>
2021-03-05 15:32:34 +0100

    Removed example file for checking JUnit setup

--- src/test/java/ExampleTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class ExampleTest {
-    @Test
-    void checkSimpleAssertion() {
-        Assertions.assertEquals(1, 1);
-    }
-}
