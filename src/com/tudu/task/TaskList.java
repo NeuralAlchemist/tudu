162f459a1d113c04ebd23dcfc3461b011f29c5e3
Yash Seeta <yashseeta94@gmail.com>
2021-03-07 14:41:17 +0100

    Added display methods for tasklist
    Tasks can now be displayed by ascending/descending order of dueDate/ project

--- src/com/tudu/task/TaskList.java
+++ src/com/tudu/task/TaskList.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class TaskList {
     // Probably a set Collection of Tasks is TaskList
     // Private fields
-    private SortedMap<String, ArrayList<Task>> projectSortedMap;
+    private TreeMap<String, ArrayList<Task>> projectSortedMap;
     private LinkedList<Task> dueDateSortedList;
 
     public TaskList() {
@@ -52,6 +52,25 @@ public class TaskList {
     public SortedMap<String, ArrayList<Task>> getSortedByProject() {
         return projectSortedMap;
     }
+
+    public void displayByDueDate(boolean ascending){
+        Iterator<Task> itr = ascending ? dueDateSortedList.iterator() : dueDateSortedList.descendingIterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next().toString());
+        }
+    }
+
+    public void displayByProject(boolean ascending){
+        NavigableSet<String> entries = ascending ? projectSortedMap.navigableKeySet() : projectSortedMap.descendingKeySet();
+        for(String entry : entries){
+            ArrayList<Task> tasksOfProject = projectSortedMap.get(entry);
+            System.out.println("tasks of project: " + entry + " total: " + tasksOfProject.size());
+            for (Task task : tasksOfProject) {
+                System.out.println(task.toString());
+            }
+        }
+    }
+
 
 
     public void displayTaskList() {
