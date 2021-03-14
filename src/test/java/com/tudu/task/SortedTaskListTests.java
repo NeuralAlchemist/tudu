d9b999e40d8f7affc685d7bc3073c6343b7c43dc
Yash Seeta <yashseeta94@gmail.com>
2021-03-14 14:04:36 +0100

    Tested saveTaskList to file for format and contents
    Added beforeeach block and cleaned up test file in structure

--- src/test/java/com/tudu/task/SortedTaskListTests.java
+++ src/test/java/com/tudu/task/SortedTaskListTests.java
@@ -2,13 +2,13 @@ package com.tudu.task;
 
 
 import org.junit.jupiter.api.*;
-
-
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.LinkedList;
-
-
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
  public class SortedTaskListTests {
@@ -26,94 +26,80 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
      private String[] dueDates = {"21-3-11 13:00\n","00-1-1 1:1\n","87-12-12 20:00\n","2050-10-23 10:0\n"};
      private String[] projectNamesForInput = {"personal\n", "work\n", "health\n", "culinary\n"};
      private String[] projectNames = {"personal", "work", "health", "culinary"};
-
-     static ArrayList<LocalDateTime> dates = new ArrayList<>(4); ;
-     public void testDates(){
-         dates.add(LocalDateTime.of(1880, 1, 1, 12, 0));
-         dates.add(LocalDateTime.of(1980, 1, 1, 12, 0));
-         dates.add(LocalDateTime.of(2080, 1, 1, 12, 0));
-         dates.add(LocalDateTime.of(2180, 1, 1, 12, 0));
+     private List<LocalDateTime> dates = Arrays.asList(LocalDateTime.of(1880, 1, 1, 12, 0),
+             LocalDateTime.of(1980, 1, 1, 12, 0),
+             LocalDateTime.of(2080, 1, 1, 12, 0),
+             LocalDateTime.of(2180, 1, 1, 12, 0));
+     private SortedTaskList sortedTaskList;
+     private final String stringPathToDatabase = "tudu-database.txt";
+     private final Path pathToDatabase = Paths.get(stringPathToDatabase);
+     private final int numberOfTaskFields = 4;
+     private final String DEFAULT_NAME = "NO NAME";
+    @BeforeEach
+    public void init(){
+         sortedTaskList = new SortedTaskList();
     }
 
      @Test
      @DisplayName("tasks added sequentially with ascending due date are sorted by ascending due date")
      public void tasksAddedSequentiallyWithAscendingDueDateAreSortedByAscendingDueDate() {
-         testDates();
-         SortedTaskList example = new SortedTaskList();
-         example.addTask("Workout", dates.get(0), false, "Exercise");
-         example.addTask("Read LOTR", dates.get(1), false, "Personal");
-         example.addTask("Collect package", dates.get(2), false, "Personal");
-         example.addTask("Write a good test", dates.get(3), false, "Work");
-         LinkedList<Task> sortedDueDate = example.getSortedByDueDate();
-         Assertions.assertAll(() -> assertEquals(dates.get(0), sortedDueDate.get(0).getDueDate()),
-                 () -> assertEquals(dates.get(1), sortedDueDate.get(1).getDueDate()),
-                 () -> assertEquals(dates.get(2), sortedDueDate.get(2).getDueDate()),
-                 () -> assertEquals(dates.get(3), sortedDueDate.get(3).getDueDate()));
-
-
+         sortedTaskList.addTask("Workout", dates.get(0), false, "Exercise");
+         sortedTaskList.addTask("Read LOTR", dates.get(1), false, "Personal");
+         sortedTaskList.addTask("Collect package", dates.get(2), false, "Personal");
+         sortedTaskList.addTask("Write a good test", dates.get(3), false, "Work");
+         LinkedList<Task> sortedDueDate = sortedTaskList.getSortedByDueDate();
+         List<LocalDateTime> actual = Arrays.asList(
+                 sortedDueDate.get(0).getDueDate(),
+                 sortedDueDate.get(1).getDueDate(),
+                 sortedDueDate.get(2).getDueDate(),
+                 sortedDueDate.get(3).getDueDate()
+         );
+         Assertions.assertEquals(dates, actual);
      }
-
-    /*@Test
-    @DisplayName("tasks added sequentially with ascending due date are sorted by ascending due date")
-    public void tasksAddedSequentiallyWithAscendingDueDateAreSortedByAscendingDueDate() {
-        testDates();
-        TaskList example = new TaskList();
-        example.addTask("Workout", dates.get(0), false, "Exercise");
-        example.addTask("Read LOTR", dates.get(1), false, "Personal");
-        example.addTask("Collect package", dates.get(2), false, "Personal");
-        example.addTask("Write a good test", dates.get(3), false, "Work");
-        LinkedList<Task> sortedDueDate = example.getSortedByDueDate();
-        Assertions.assertAll(() -> assertEquals(dates.get(0), sortedDueDate.get(0).getDueDate()),
-                () -> assertEquals(dates.get(1), sortedDueDate.get(1).getDueDate()),
-                () -> assertEquals(dates.get(2), sortedDueDate.get(2).getDueDate()),
-                () -> assertEquals(dates.get(3), sortedDueDate.get(3).getDueDate()));
-
-
-    }*/
 
      @Test
      @DisplayName("tasks added sequentially with descending due date are sorted by ascending due date")
      void tasksAddedSequentiallyWithDescendingDueDateAreSortedByAscendingDueDate() {
-         testDates();
-         SortedTaskList example = new SortedTaskList();
-         example.addTask("Workout", dates.get(3), false, "Exercise");
-         example.addTask("Read LOTR", dates.get(2), false, "Personal");
-         example.addTask("Collect package", dates.get(1), false, "Personal");
-         example.addTask("Write a good test", dates.get(0), false, "Work");
-         LinkedList<Task> sortedDueDate = example.getSortedByDueDate();
-         Assertions.assertAll(() -> assertEquals(dates.get(0), sortedDueDate.get(0).getDueDate()),
-                 () -> assertEquals(dates.get(1), sortedDueDate.get(1).getDueDate()),
-                 () -> assertEquals(dates.get(2), sortedDueDate.get(2).getDueDate()),
-                 () -> assertEquals(dates.get(3), sortedDueDate.get(3).getDueDate()));
+         sortedTaskList.addTask("Workout", dates.get(3), false, "Exercise");
+         sortedTaskList.addTask("Read LOTR", dates.get(2), false, "Personal");
+         sortedTaskList.addTask("Collect package", dates.get(1), false, "Personal");
+         sortedTaskList.addTask("Write a good test", dates.get(0), false, "Work");
+         LinkedList<Task> sortedDueDate = sortedTaskList.getSortedByDueDate();
+         List<LocalDateTime> actual = Arrays.asList(
+                 sortedDueDate.get(0).getDueDate(),
+                 sortedDueDate.get(1).getDueDate(),
+                 sortedDueDate.get(2).getDueDate(),
+                 sortedDueDate.get(3).getDueDate()
+         );
+         Assertions.assertEquals(dates, actual);
      }
 
 
      @Test
      @DisplayName("tasks added sequentially with random due date are sorted by ascending due date")
      void tasksAddedSequentiallyWithRandomDueDateAreSortedByAscendingDueDate() {
-         testDates();
-         SortedTaskList example = new SortedTaskList();
-         example.addTask("Workout", dates.get(2), false, "Exercise");
-         example.addTask("Read LOTR", dates.get(0), false, "Personal");
-         example.addTask("Collect package", dates.get(3), false, "Personal");
-         example.addTask("Write a good test", dates.get(1), false, "Work");
-         LinkedList<Task> sortedDueDate = example.getSortedByDueDate();
-         Assertions.assertAll(() -> assertEquals(dates.get(0), sortedDueDate.get(0).getDueDate()),
-                 () -> assertEquals(dates.get(1), sortedDueDate.get(1).getDueDate()),
-                 () -> assertEquals(dates.get(2), sortedDueDate.get(2).getDueDate()),
-                 () -> assertEquals(dates.get(3), sortedDueDate.get(3).getDueDate()));
+         sortedTaskList.addTask("Workout", dates.get(2), false, "Exercise");
+         sortedTaskList.addTask("Read LOTR", dates.get(0), false, "Personal");
+         sortedTaskList.addTask("Collect package", dates.get(3), false, "Personal");
+         sortedTaskList.addTask("Write a good test", dates.get(1), false, "Work");
+         LinkedList<Task> sortedDueDate = sortedTaskList.getSortedByDueDate();
+         List<LocalDateTime> actual = Arrays.asList(
+                 sortedDueDate.get(0).getDueDate(),
+                 sortedDueDate.get(1).getDueDate(),
+                 sortedDueDate.get(2).getDueDate(),
+                 sortedDueDate.get(3).getDueDate()
+         );
+         Assertions.assertEquals(dates, actual);
      }
 
      @Test
      @DisplayName("tasks added sequentially with duplicate due dates in random order are sorted by ascending due date")
      void tasksAddedSequentiallyWithDuplicateDueDatesInRandomOrderAreSortedByAscendingDueDate() {
-         testDates();
-         SortedTaskList example = new SortedTaskList();
-         example.addTask("Workout", dates.get(2), false, "Exercise");
-         example.addTask("Read LOTR", dates.get(0), false, "Personal");
-         example.addTask("Collect package", dates.get(1), false, "Personal");
-         example.addTask("Write a good test", dates.get(1), false, "Work");
-         LinkedList<Task> sortedDueDate = example.getSortedByDueDate();
+         sortedTaskList.addTask("Workout", dates.get(2), false, "Exercise");
+         sortedTaskList.addTask("Read LOTR", dates.get(0), false, "Personal");
+         sortedTaskList.addTask("Collect package", dates.get(1), false, "Personal");
+         sortedTaskList.addTask("Write a good test", dates.get(1), false, "Work");
+         LinkedList<Task> sortedDueDate = sortedTaskList.getSortedByDueDate();
          Assertions.assertAll(() -> assertEquals(dates.get(0), sortedDueDate.get(0).getDueDate()),
                  () -> assertEquals(dates.get(1), sortedDueDate.get(1).getDueDate()),
                  () -> assertEquals(dates.get(1), sortedDueDate.get(2).getDueDate()),
@@ -123,22 +109,63 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
      @Test
      @DisplayName("empty task name results in default name for task")
      void emptyTaskNameResultsInDefaultNameForTask() {
-         testDates();
-         SortedTaskList example = new SortedTaskList();
-         example.addTask("", dates.get(2), false, "Empty");
-         LinkedList<Task> tasks = example.getSortedByDueDate();
-         Assertions.assertAll(() -> assertEquals(1, example.getNumberOfTasks()),
+         sortedTaskList.addTask("", dates.get(2), false, "Empty");
+         LinkedList<Task> tasks = sortedTaskList.getSortedByDueDate();
+         Assertions.assertAll(() -> assertEquals(1, sortedTaskList.getNumberOfTasks()),
                  () -> assertEquals("NO NAME", tasks.get(0).getName()));
      }
 
      @Test
      @DisplayName("empty project name results in default name for project")
      void emptyProjectNameResultsInDefaultNameForProject() {
-         testDates();
-         SortedTaskList example = new SortedTaskList();
-         example.addTask("Empty", dates.get(2), false, "");
-         Assertions.assertEquals(true, example.getSortedByProject().containsKey("NO NAME"));
-         assertEquals(1, example.getNumberOfTasks());
+         sortedTaskList.addTask("Empty", dates.get(2), false, "");
+         Assertions.assertEquals(true, sortedTaskList.getSortedByProject().containsKey("NO NAME"));
+         assertEquals(1, sortedTaskList.getNumberOfTasks());
      }
 
+     @Test
+     @DisplayName("saved file contains all the entered task details in newline delimited form")
+     void savedFileContainsAllTheEnteredTaskDetailsInNewlineDelimitedForm() {
+         //Delete contents of file to start with a clean file
+         try {
+             new FileOutputStream(stringPathToDatabase).close();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+         sortedTaskList.addTask("", localDueDates[3], false, "");
+         sortedTaskList.addTask(taskNames[2], localDueDates[2],  false, projectNames[2]);
+         int numberOfTasksAdded = 2;
+         assertEquals(2, sortedTaskList.getNumberOfTasks());
+         ArrayList<String> content;
+         sortedTaskList.saveTaskListToFile(stringPathToDatabase);
+         try {
+             content = new ArrayList<>(Files.readAllLines(pathToDatabase));
+             assertEquals(numberOfTasksAdded * numberOfTaskFields, content.size());
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+
+     @Test
+     @DisplayName("saved file contains the inputted task details in due date sorted order")
+     void savedFileContainsTheInputtedTaskDetailsInDueDateSortedOrder() {
+         //Delete contents of file to start with a clean file
+         try {
+             new FileOutputStream(stringPathToDatabase).close();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+         sortedTaskList.addTask("", localDueDates[3], false, "");
+         sortedTaskList.addTask(taskNames[2], localDueDates[2],  false, projectNames[2]);
+         List<String> actual = Arrays.asList(taskNames[2], localDueDates[2].toString(), TaskStatus.UNSTARTED.toString(), projectNames[2],
+                 DEFAULT_NAME, localDueDates[3].toString(), TaskStatus.UNSTARTED.toString(), DEFAULT_NAME);
+         ArrayList<String> content;
+         sortedTaskList.saveTaskListToFile(stringPathToDatabase);
+         try {
+             content = new ArrayList<>(Files.readAllLines(pathToDatabase));
+             Assertions.assertAll(() -> Assertions.assertLinesMatch(actual, content));
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
 }
