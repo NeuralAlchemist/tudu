40ac3a172ad796856c140b6a7a5046aae1d317e1
Yash Seeta <yashseeta94@gmail.com>
2021-03-14 17:20:46 +0100

    Updated addTask method to take in integer value relating to enum TaskStatus

--- src/com/tudu/task/TaskListObject.java
+++ src/com/tudu/task/TaskListObject.java
@@ -3,7 +3,7 @@ package com.tudu.task;
 import java.time.LocalDateTime;
 
 abstract class TaskListObject {
-    protected abstract void addTask(String taskName, LocalDateTime dueDate, boolean status, String project);
+    protected abstract void addTask(String taskName, LocalDateTime dueDate, int status, String project);
     protected abstract void displayByDueDate(boolean ascending);
     protected abstract void displayByProject(boolean ascending);
     // Method to edit fields of a Task
