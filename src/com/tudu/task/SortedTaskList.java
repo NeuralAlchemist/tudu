c6cfa2dc9a0df82f6baac15d95b7e34dafd56715
Yash Seeta <yashseeta94@gmail.com>
2021-03-15 16:41:30 +0100

    Split loading from file into two seperate functions
    Added method that combines these two. addTask method returns the added task now.

--- src/com/tudu/task/SortedTaskList.java
+++ src/com/tudu/task/SortedTaskList.java
@@ -4,14 +4,16 @@ import org.fusesource.jansi.Ansi;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+
 import static org.fusesource.jansi.Ansi.ansi;
 
-public class SortedTaskList extends TaskListObject{
+public class SortedTaskList extends TaskListObject {
     // Probably a set Collection of Tasks is TaskList
     // Private fields
     private TreeMap<String, ArrayList<Task>> projectSortedMap;
@@ -27,7 +29,8 @@ public class SortedTaskList extends TaskListObject{
     // Methods : ~~addTask~~, editTask, markAsDone, removeTask(IF: support removeAll, IF: support removeAllProjectFlag)
     // update to check for already present tasks and return false if not added
     @Override
-    protected void addTask(String taskName, LocalDateTime dueDate, int status, String project) {
+    protected Task addTask(String taskName, LocalDateTime dueDate, int status, String project) {
+        Task result;
         Task task = new Task(taskName, dueDate, status, project);
         String projectName = task.getProject();
         boolean addedToProjectSortedMap = false;
@@ -46,16 +49,16 @@ public class SortedTaskList extends TaskListObject{
         if (dueDateSortedList.isEmpty()) {
             dueDateSortedList.add(task);
             addedToDueDateSortedList = true;
-        } else if(!dueDateSortedList.contains(task)) {
+        } else if (!dueDateSortedList.contains(task)) {
             Iterator<Task> listItr = dueDateSortedList.descendingIterator();
-            int currentIndex = dueDateSortedList.size()-1;
-            while(listItr.hasNext()){
+            int currentIndex = dueDateSortedList.size() - 1;
+            while (listItr.hasNext()) {
                 Task current = listItr.next();
-                if(task.getDueDate().isAfter(current.getDueDate())){
-                    dueDateSortedList.add(currentIndex+1, task);
+                if (task.getDueDate().isAfter(current.getDueDate())) {
+                    dueDateSortedList.add(currentIndex + 1, task);
                     addedToDueDateSortedList = true;
                     break;
-                } else if(currentIndex == 0){
+                } else if (currentIndex == 0) {
                     dueDateSortedList.offerFirst(task);
                     addedToDueDateSortedList = true;
                 }
@@ -64,13 +67,14 @@ public class SortedTaskList extends TaskListObject{
         } else {
             addedToDueDateSortedList = false;
         }
-        if(addedToDueDateSortedList && addedToProjectSortedMap){
+        if (addedToDueDateSortedList && addedToProjectSortedMap) {
             numberOfTasks++;
             System.out.println("Task has been added");
-            //Implement boolean return
+            result = task;
         } else {
-            // Implement boolean return
+            result = null;
         }
+        return result;
     }
 
 
@@ -85,17 +89,17 @@ public class SortedTaskList extends TaskListObject{
 
     // Methods : sortByDate(ascending/descending), sortByProjectFlag(ascending/descending)
     @Override
-    protected void displayByDueDate(boolean ascending){
+    protected void displayByDueDate(boolean ascending) {
         Iterator<Task> itr = ascending ? dueDateSortedList.iterator() : dueDateSortedList.descendingIterator();
-        while(itr.hasNext()){
+        while (itr.hasNext()) {
             System.out.println(itr.next().toString());
         }
     }
 
     @Override
-    protected void displayByProject(boolean ascending){
+    protected void displayByProject(boolean ascending) {
         NavigableSet<String> entries = ascending ? projectSortedMap.navigableKeySet() : projectSortedMap.descendingKeySet();
-        for(String entry : entries){
+        for (String entry : entries) {
             ArrayList<Task> tasksOfProject = projectSortedMap.get(entry);
             System.out.println("tasks of project: " + entry + " total: " + tasksOfProject.size());
             for (Task task : tasksOfProject) {
@@ -104,25 +108,25 @@ public class SortedTaskList extends TaskListObject{
         }
     }
 
-    protected ArrayList<Task> findTaskByName(String taskName){
+    protected ArrayList<Task> findTaskByName(String taskName) {
         ArrayList<Task> listOfTasksFound = new ArrayList<>(10);
         Iterator<Task> itr = dueDateSortedList.iterator();
         Task current;
-        while(itr.hasNext()){
+        while (itr.hasNext()) {
             current = itr.next();
-            if(current.getName().equalsIgnoreCase(taskName)){
+            if (current.getName().equalsIgnoreCase(taskName)) {
                 listOfTasksFound.add(current);
             }
         }
-        return listOfTasksFound.isEmpty()? null : listOfTasksFound;
+        return listOfTasksFound.isEmpty() ? null : listOfTasksFound;
     }
 
-    protected int getNumberOfTasks(){
+    protected int getNumberOfTasks() {
         return numberOfTasks;
     }
 
     @Override
-    protected void saveTaskListToFile(String stringPathToDatabase){
+    protected void saveTaskListToFile(String stringPathToDatabase) {
         File databaseFile = new File(stringPathToDatabase);
         PrintWriter writer;
         try {
@@ -133,7 +137,7 @@ public class SortedTaskList extends TaskListObject{
             writer = new PrintWriter(new FileOutputStream(databaseFile, true));
             Iterator<Task> itr = dueDateSortedList.iterator();
             Task current;
-            while(itr.hasNext()){
+            while (itr.hasNext()) {
                 current = itr.next();
                 writer.println(current.getName());
                 writer.println(current.getDueDate());
@@ -143,57 +147,90 @@ public class SortedTaskList extends TaskListObject{
             writer.close();
         } catch (FileNotFoundException e) {
             System.out.println("Cannot save to file, as file not found!");
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    protected void loadTaskListFromFile(String stringPathToDatabase){
+    protected boolean searchForDatabaseFile(String stringPathToDatabase) {
         System.out.print("Searching for database ..");
+        // Jansi code to blink the string "." slowly
         System.out.print(ansi().a(Ansi.Attribute.BLINK_SLOW).a("."));
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        //System.out.print(ansi().reset());
-        //System.out.flush();
+        // Jansi code to clear/reset the attribute of blinking
         System.out.print(ansi().reset());
+        // Jansi code to erase everything behind the current cursor
+        // and set cursor to the beginning of the current line
         System.out.print(ansi().eraseLine(Ansi.Erase.BACKWARD).cursorToColumn(1));
-        //System.out.print(ansi().eraseLine(Ansi.Erase.BACKWARD).cursorToColumn(1).reset());
         System.out.println("Searching for database ...");
+        return new File(stringPathToDatabase).exists();
+    }
+
+    protected boolean loadTaskList(String stringPathToDatabase) {
+        boolean result;
+        if (searchForDatabaseFile(stringPathToDatabase)) {
+            result = loadTaskListFromFile(stringPathToDatabase);
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    protected void printLoadingMessage() {
+        // Fancy loading message
+        String[] loadingPattern = {"-", "\\", "|", "/", "-"};
+        System.out.print("Loading tasks ");
+        System.out.print("/");
+        for (int i = 0; i < loadingPattern.length; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print(loadingPattern[i % (loadingPattern.length)]);
+            System.out.print(ansi().cursorLeft(1));
+        }
+        System.out.println("");
+    }
+
+
+    /**
+     * Loads tasklist details from a local text database to this SortedTaskList.
+     * @param stringPathToDatabase is the relative path to the local database
+     * @return Returns {@code true} if all the tasks have been loaded successfully;
+     *          {@code false} otherwise
+     * */
+    protected boolean loadTaskListFromFile(String stringPathToDatabase) {
+        boolean result = true;
         File databaseFile = new File(stringPathToDatabase);
         Path pathToDatabase = Paths.get(stringPathToDatabase);
         ArrayList<String> content;
-        if(databaseFile.exists()){
-            System.out.print("Loading tasks ");
+        // Load from file only if it contains data
+        if (databaseFile.length() > -1) {
+            printLoadingMessage();
             try {
-                String[] loadingPattern = {"-","\\","|","/","-"};
-                System.out.print("/");
-                for(int i = 0; i < loadingPattern.length ; i++){
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.print(loadingPattern[i% (loadingPattern.length)]);
-                    System.out.print(ansi().cursorLeft(1));
-
-                }
-                System.out.println("");
                 content = new ArrayList<>(Files.readAllLines(pathToDatabase));
                 DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
-                for(int i = 0; i < content.size(); i = i+4){
-                    addTask(content.get(i),
-                            LocalDateTime.parse(content.get(i+1), formatter),
-                            Integer.parseInt(content.get(i+2))+1,
-                            content.get(i+3));
+                Task current;
+                for (int i = 0; i < content.size(); i = i + 4) {
+                    current = addTask(content.get(i),
+                            // Due date is stored as LocalDateTime's ISO_DATE_TIME format
+                            // it needs to be parsed to LocalDateTime object to be added
+                            LocalDateTime.parse(content.get(i + 1), formatter),
+                            Integer.parseInt(content.get(i + 2)) + 1,
+                            content.get(i + 3));
+                    result = result & (current != null);
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                result = false;
             }
         }
+        return result;
     }
 
     /*public void editByProject(){
