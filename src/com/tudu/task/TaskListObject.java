dd86559613003423cde097609b461873d0e8d0bd
Yash Seeta <yashseeta94@gmail.com>
2021-03-16 10:36:53 +0100

    Removed display methods
     Better implemented in the SortedTaskListUI

--- src/com/tudu/task/TaskListObject.java
+++ src/com/tudu/task/TaskListObject.java
@@ -6,8 +6,6 @@ import java.util.NavigableSet;
 
 abstract class TaskListObject {
     protected abstract Task addTask(String taskName, LocalDateTime dueDate, int status, String project);
-    protected abstract LinkedList<Task> displayByDueDate(boolean ascending);
-    protected abstract NavigableSet<String> displayByProject(boolean ascending);
     // Method to edit fields of a Task
     // Method to mark task as done
     // Method to remove tasks
