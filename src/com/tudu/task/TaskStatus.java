1db556f51699f626d76deafa3e3948cf2bb54fb2
Yash Seeta <yashseeta94@gmail.com>
2021-03-18 19:36:20 +0100

    Added documentation

--- src/com/tudu/task/TaskStatus.java
+++ src/com/tudu/task/TaskStatus.java
@@ -1,9 +1,21 @@
 package com.tudu.task;
 
+/**
+ * TaskStatus is a enum representing the 3 stages of a task - Ongoing, Done and Unstarted.
+ */
 public enum TaskStatus {
-    //Enum class perhaps
-    // TaskStatus can be ongoing, done, unstarted
-    ONGOING, DONE, UNSTARTED;
+    /**
+     * The singleton instance for representing an ongoing task.
+     */
+    ONGOING,
 
+    /**
+     * The singleton instance for representing a finished task.
+     */
+    DONE,
 
+    /**
+     * The singleton instance for representing a task that has not begun.
+     */
+    UNSTARTED;
 }
