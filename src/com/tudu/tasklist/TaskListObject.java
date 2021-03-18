b540488284ac002d79832c70b8f6ecbbb289c8ad
Yash Seeta <yashseeta94@gmail.com>
2021-03-18 20:15:21 +0100

    Updated addTask and setTask to use TaskStatus

--- src/com/tudu/tasklist/TaskListObject.java
+++ src/com/tudu/tasklist/TaskListObject.java
@@ -1,14 +1,15 @@
 package com.tudu.tasklist;
 
 import com.tudu.task.Task;
+import com.tudu.task.TaskStatus;
 
 import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.NavigableSet;
 
 abstract class TaskListObject {
-    protected abstract Task addTask(String taskName, LocalDateTime dueDate, int status, String project);
-    protected abstract void setTask(String taskName, LocalDateTime dueDate, int status, String project, Task oldTask);
+    protected abstract Task addTask(String taskName, LocalDateTime dueDate, TaskStatus status, String project);
+    protected abstract void setTask(String taskName, LocalDateTime dueDate, TaskStatus status, String project, Task oldTask);
     protected abstract Task markTaskAsDone(Task task);
     protected abstract boolean removeTask(Task task);
     protected abstract void saveTaskListToFile(String stringPathToDatabase);
